1887f795ab1d3a39e908f1a5bb9af9bbac6ee52f
DocCreeps <dorian1998dino@hotmail.com>
2023-06-23 13:35:34 +0200

    🎨 design: sidemenu and start forms

--- resources/views/livewire/create.blade.php
+++ resources/views/livewire/create.blade.php
@@ -2,37 +2,37 @@
     <div class="card-body">
         <form>
             @csrf
-            <div class="form-group mb-3">
-                <label for="nom">Nom:</label>
-                <input type="text" class="form-control @error('nom') is-invalid @enderror" id="nom" placeholder="Enter nom" wire:model="nom">
+            <div class="mb-3">
+                <label for="nom" class="block mb-1">Nom:</label>
+                <input type="text" class="form-input @error('nom') border-red-500 @enderror" id="nom" placeholder="Enter nom" wire:model="nom">
                 @error('nom')
-                <span class="text-danger">{{ $message }}</span>
+                <span class="text-red-500">{{ $message }}</span>
                 @enderror
             </div>
-            <div class="form-group mb-3">
-                <label for="prenom">Prénom:</label>
-                <input type="text" class="form-control @error('prenom') is-invalid @enderror" id="prenom" placeholder="Enter prenom" wire:model="prenom">
+            <div class="mb-3">
+                <label for="prenom" class="block mb-1">Prénom:</label>
+                <input type="text" class="form-input @error('prenom') border-red-500 @enderror" id="prenom" placeholder="Enter prenom" wire:model="prenom">
                 @error('prenom')
-                <span class="text-danger">{{ $message }}</span>
+                <span class="text-red-500">{{ $message }}</span>
                 @enderror
             </div>
-            <div class="form-group mb-3">
-                <label for="description">Description:</label>
-                <textarea class="form-control @error('description') is-invalid @enderror" id="description" wire:model="description" placeholder="Enter Description"></textarea>
+            <div class="mb-3">
+                <label for="description" class="block mb-1">Description:</label>
+                <textarea class="form-textarea @error('description') border-red-500 @enderror" id="description" wire:model="description" placeholder="Enter Description"></textarea>
                 @error('description')
-                <span class="text-danger">{{ $message }}</span>
+                <span class="text-red-500">{{ $message }}</span>
                 @enderror
             </div>
-            <div class="form-group mb-3">
-                <label for="image">Photo:</label>
-                <input type="file" class="form-control @error('image') is-invalid @enderror" id="image" wire:model="image">
-                @error('Image')
-                <span class="text-danger">{{ $message }}</span>
+            <div class="mb-3">
+                <label for="image" class="block mb-1">Photo:</label>
+                <input type="file" class="form-input @error('image') border-red-500 @enderror" id="image" wire:model="image">
+                @error('image')
+                <span class="text-red-500">{{ $message }}</span>
                 @enderror
             </div>
-            <div class="d-grid gap-2">
-                <button wire:click.prevent="storeStar()" class="btn btn-success btn-block">Save</button>
-                <button wire:click.prevent="cancelStar()" class="btn btn-secondary btn-block">Cancel</button>
+            <div class="grid gap-2">
+                <button wire:click.prevent="storeStar()" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">Save</button>
+                <button wire:click.prevent="cancelStar()" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">Cancel</button>
             </div>
         </form>
     </div>
--- resources/views/livewire/star.blade.php
+++ resources/views/livewire/star.blade.php
@@ -1,68 +1,38 @@
-<div>
-    <div class="col-md-8 mb-2">
-        @if(session()->has('success'))
-            <div class="alert alert-success" role="alert">
-                {{ session()->get('success') }}
-            </div>
-        @endif
-        @if(session()->has('error'))
-            <div class="alert alert-danger" role="alert">
-                {{ session()->get('error') }}
-            </div>
-        @endif
-        @if($addStar)
-            @include('livewire.create')
-        @endif
-        @if($updateStar)
-            @include('livewire.update')
-        @endif
-    </div>
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-body">
-                @if(!$addStar)
-                    <button wire:click="addStar()" class="btn btn-primary btn-sm float-right">Add New Star</button>
-                @endif
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th>nom</th>
-                            <th>Prénom</th>
-                            <th>Description</th>
-                            <th>image</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @if (isset($star) && count($star) > 0)
-                            @foreach ($star as $stars)
-                                <tr>
-                                    <td>
-                                        {{ $stars->nom }}
-                                    </td>
-                                    </td>
-                                    <td>
-                                        {{ $stars->description }}
-                                    </td>
-                                    <td>
-                                        <button wire:click="editStar({{ $stars->id }})" class="btn btn-primary btn-sm">Edit</button>
-                                        <button onclick="deleteStar({{ $stars->id }})" class="btn btn-danger btn-sm">Delete</button>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        @else
-                            <tr>
-                                <td colspan="3" align="center">
-                                    No Star Found.
-                                </td>
-                            </tr>
-                        @endif
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="parent md:h-screen md:grid md:grid-cols-6">
+    <div class="sidebar md:col-span-1 text-center border-e bg-white ">
+{{--                <div class="flex h-screen flex-col justify-between items-center border-e bg-white w-1/6">--}}
+                    <div class="px-4 py-6">
+                        <h2 class="text-2xl font-semibold text-gray-800 mb-5">Listes des Stars</h2>
 
+                        @if(!$addStar)
+                            <button wire:click="addStar()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full">Add New Star</button>
+                        @endif
+
+                        <ul class="mt-6 space-y-1">
+                            @foreach ($stars as $star)
+                                <li class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"> {{ $star->nom . ' ' . $star->prenom }} </a></li>
+
+                            @endforeach
+                        </ul>
+                    </div>
+            </div>
+    <div class="main bg-grey-400 md:col-span-5">
+                    @if(session()->has('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session()->get('success') }}
+                        </div>
+                    @endif
+                    @if(session()->has('error'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session()->get('error') }}
+                        </div>
+                    @endif
+                    @if($addStar)
+                        @include('livewire.create')
+                    @endif
+                    @if($updateStar)
+                        @include('livewire.update')
+                    @endif
+        </div>
 </div>
+
